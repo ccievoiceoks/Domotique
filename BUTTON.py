e47cfe6a52421f6b54d40424eea93e626ae4635a
oks <olivier@ksiazek.be>
2017-12-15 13:18:26 +0100

    New While Condition

--- BUTTON.py
+++ BUTTON.py
@@ -7,17 +7,21 @@ GPIO.setup(18,GPIO.OUT)
 GPIO.setup(4,GPIO.OUT)
 input = GPIO.input(21)
 print input
-while True:
+while counters<180:
       if (GPIO.input(21)):
               print GPIO.input(21)
               print("Open Contact")
        		  GPIO.output(4,GPIO.LOW)
 			  GPIO.output(18,GPIO.HIGH)
               time.sleep(2)
+              counters+=
       else:
               print GPIO.input(21)
               print ("Closed Contact")
               GPIO.output(18,GPIO.LOW)
 			  GPIO.output(4,GPIO.HIGH)
               time.sleep(2)
+              counters+=
+GPIO.output(4,GPIO.LOW)
+GPIO.output(18,GPIO.LOW)
 

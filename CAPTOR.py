52bed532c897410e03b6b5276d3f55ccf81e8356
oks <olivier@ksiazek.be>
2017-12-15 14:39:37 +0100

    Update on Comment

--- CAPTOR.py
+++ CAPTOR.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-// Magnetic Contact
+# Magnetic Contact
 GPIO.setup(21,GPIO.IN)
 GPIO.setup(26,GPIO.IN)
-// Output Contact Led 
+#Output Contact Led 
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(4,GPIO.OUT)
 

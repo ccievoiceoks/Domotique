820e3567d9ef349680f3161cbb3df648d12a8625
Olivier Ksiazek <Olivier@Ksiazek.be>
2017-12-15 09:46:09 +0100

    First Commit

---
+++ LED.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+print "Circuit on"
+GPIO.output(4,GPIO.LOW)
+GPIO.output(18,GPIO.HIGH)
+time.sleep(20)
+print "Circuit off"
+GPIO.output(4,GPIO.HIGH)
+GPIO.output(18,GPIO.LOW)
+time.sleep(20)

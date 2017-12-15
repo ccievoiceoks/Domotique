912b9e45135faee8966f3b636c841be82aa39aba
oks <olivier@ksiazek.be>
2017-12-15 12:11:49 +0100

    Introduction of LED in script with Status

--- BUTTON.py
+++ BUTTON.py
@@ -3,15 +3,21 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(21,GPIO.IN)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
 input = GPIO.input(21)
 print input
 while True:
       if (GPIO.input(21)):
               print GPIO.input(21)
               print("Open Contact")
+       		  GPIO.output(4,GPIO.LOW)
+			  GPIO.output(18,GPIO.HIGH)
               time.sleep(2)
       else:
               print GPIO.input(21)
               print ("Closed Contact")
+              GPIO.output(18,GPIO.LOW)
+			  GPIO.output(4,GPIO.HIGH)
               time.sleep(2)
 

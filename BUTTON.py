8cdea6251a93107160585f6698d263a78a4b5db6
Olivier Ksiazek <Olivier@Ksiazek.be>
2017-12-15 14:30:25 +0100

    New Condition Max Counters Introduced

--- BUTTON.py
+++ BUTTON.py
@@ -8,23 +8,24 @@ GPIO.setup(4,GPIO.OUT)
 input = GPIO.input(21)
 print input
 counters=0
-while counters<120:
+max_counters=int(raw_input(" How many times do I need to iterate the loop? :"))
+while counters<max_counters:
       if (GPIO.input(21)):
-              print GPIO.input(21)
+              print "Value Contact =====> ",GPIO.input(21)
               print("Open Contact")
        	      GPIO.output(4,GPIO.LOW)
 	      GPIO.output(18,GPIO.HIGH)
               time.sleep(1)
               counters+=1
-              print "Counters => "counters
+              print "Counters => " + str(counters)
       else:
-              print GPIO.input(21)
+              print "Value Contact =====> ",GPIO.input(21)
               print ("Closed Contact")
               GPIO.output(18,GPIO.LOW)
 	      GPIO.output(4,GPIO.HIGH)
               time.sleep(1)
               counters+=1
-              print "Counters => "counters
+              print "Counters => " + str(counters)
 GPIO.output(4,GPIO.LOW)
 GPIO.output(18,GPIO.LOW)
 print " "

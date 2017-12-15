2b027b95983a8e3b511d2ec60e2f36092d70a109
oks <olivier@ksiazek.be>
2017-12-15 14:36:24 +0100

    New Script Captor Introduced

---
+++ CAPTOR.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+// Magnetic Contact
+GPIO.setup(21,GPIO.IN)
+GPIO.setup(26,GPIO.IN)
+// Output Contact Led 
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+
+counters=0
+max_counters=int(raw_input(" How many times do I need to iterate the loop? :"))
+while counters<max_counters:
+  if (GPIO.input(21)):
+    print "Value Contact =====> ",GPIO.input(21)
+    print("Open Contact")
+    GPIO.output(4,GPIO.LOW)
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    counters+=1
+    print "Counters => " + str(counters)
+  else:
+    print "Value Contact =====> ",GPIO.input(21)
+    print ("Closed Contact")
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(4,GPIO.HIGH)
+    time.sleep(1)
+    counters+=1
+    print "Counters => " + str(counters)
+    GPIO.output(4,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
+    print " "
+    print " Script Terminated" 
